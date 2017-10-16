4c80af496c58df4b2e9c08415e49696a03a99f92
eagobert <eagobert@hotmail.com>
2017-10-16 16:46:03 -0500

    make changes to supplies database.

---
+++ js/supplies-factory.js
@@ -0,0 +1,42 @@
+//Define objects for the supply database
+
+const smallCrate = {
+    "name": "Small Shipping Crate",
+    "type": "crate",
+    "Price":,
+    "image":
+}
+
+const mediumCrate = {
+    "name": "Medium Shipping Crate",
+    "type": "crate",
+    "Price":,
+    "image":
+}
+
+const largeCrate = {
+    "name": "Large Shipping Crate",
+    "type": "crate",
+    "Price":,
+    "image":
+}
+
+const smallPackMat = {
+    "name": "Small Packing Materials",
+    "type": "packing materials",
+    "Price":,
+    "image":
+}
+
+const medPackMat = {
+    "name": "Medium Packing Materials",
+    "type": "packing materials,
+    "Price":,
+    "image":
+}
+
+const largePackMat = {
+    "name": "Large Packing Materials,
+    "type": "packing materials",
+    "image":
+}
