51c2e79bd0239852193f51a98a7521b7ba368dd5
Greg Lawrence <gregaudio@gmail.com>
2017-10-16 10:15:30 -0500

    initial commit, made js and css files, added commented out code flow in js

---
+++ js/items-factory.js
@@ -0,0 +1,40 @@
+/* if (items database exists) {
+  //load local storage and assign to a variable     
+
+} 
+    else {
+        // generate new items database
+
+        const items = {};
+
+        const smallItem = {
+            "name": "small",
+            "price": 30,
+        }
+
+        const mediumItem = {
+            "name": "medium",
+            "price": 60,
+        }
+
+        const largeItem = {
+            "name": "large",
+            "price": 100,
+        }
+
+        items.smallItem = smallItem;
+        items.mediumItem = mediumItem;
+        items.largeItem = largeItem;
+
+    }
+*/
+
+
+
+
+// get number info from text box on items.html via document.getElementById("") 
+
+// create a .onclick() function to push the items and quantity to cart database
+
+// 
+
