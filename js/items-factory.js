c0cf4c974fa4c8ee10b93967d4c0ee080a80d21f
Greg Lawrence <gregaudio@gmail.com>
2017-10-16 15:05:40 -0500

    created items page, constructed database, populated html page with database, added some temporary styling

--- js/items-factory.js
+++ js/items-factory.js
@@ -1,4 +1,4 @@
-if (localStorage.getItem("itemsDatabaseString") === null) {
+if (localStorage.getItem("itemsDatabaseString") !== null) {
   // let items-controller.js file handle database that already exists    
    
 } 
@@ -6,34 +6,42 @@ else {
     // generate new items database
 
     const items = {};
+    const itemsArray = [];
 
     const smallItem = {
         "name": "small",
         "price": 30,
+        "picture": "/images/Rhino_small_web.jpg"
     }
 
     const mediumItem = {
         "name": "medium",
-        "price": 60,
+        "price": 75,
+        "picture": "/images/Rhino_med_web.jpg"
     }
 
     const largeItem = {
         "name": "large",
-        "price": 100,
+        "price": 250,
+        "picture": "/images/Rhino_large_web.jpg"
+    }
+    const extraLargeItem = {
+        "name": "XL",
+        "price": 1500,
+        "picture": "/images/Rhino_xl_web.jpg"
     }
 
-    items.smallItem = smallItem;
-    items.mediumItem = mediumItem;
-    items.largeItem = largeItem;
+    const description = {
+        "items-description": "We will help you move a Rhinocerus Statue of any size. Just select the size and the number of statues you'd like to use below and we will help you."
+    }
+
+    itemsArray.push(smallItem, mediumItem, largeItem, extraLargeItem);
+    items.items = itemsArray;
+    items.description = description;
 
     // store items database in local storage
     let itemsString = JSON.stringify(items);
-    localStorage.setItem("itemsDatabaseString", itemsDatabaseString);
+    localStorage.setItem("itemsString", itemsString);
 
 }
-
-
-// if (localStorage.getItem("cart") === null) {
-//     window.location = "http://localhost:8080/";
-// }
 
