bb906c9db9563708800921ace878112ea1aa34d5
Gward2489 <gww8804@gmail.com>
2017-10-17 14:42:41 -0500

    updated styles made buttons work

--- js/index-controller.js
+++ js/index-controller.js
@@ -5,5 +5,6 @@ let indexMarker = document.getElementById("indexId")
 for (let i = 0; i < storedIndex.length; i++) {
     let currentIndex = storedIndex[i]
     indexMarker.innerHTML += `<div class="${currentIndex.class}"><p>${currentIndex.title}</p>
-    <button class="${currentIndex.subclass}" onclick="${currentIndex.extra};">RHINO TIME</button></div>`
+    <button class="${currentIndex.subclass}" onclick="location.href = './items.html'">RHINO TIME</button></div>`
 } 
+
