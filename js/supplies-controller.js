0e9bf46b9d682698b09553e9d98ee9a0a7c5353d
eagobert <eagobert@hotmail.com>
2017-10-16 20:53:03 -0500

    change variables in supplies-controller

--- js/supplies-controller.js
+++ js/supplies-controller.js
@@ -1,5 +1,8 @@
 /* --Read data using JSON.parse-- */
 
 //Create a constant named "storedSupplyInventory" to read data ("supplyInventory") in memory
-const storedSupplyInventory = JSON.parse(localStorage.getItem("supplyInventory"))
+const storedSupplyInventory = localStorage.getItem("supplyInventory")
+
+//Create a variable named "supplies" to store the read data in "storedSupplyInventory"
+let supplies = JSON.parse(storedSupplyInventory)
 
