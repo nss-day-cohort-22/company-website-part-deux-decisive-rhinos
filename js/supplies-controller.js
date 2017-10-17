874da2edf138ab88fe1946b201eeb33d5dd09062
eagobert <eagobert@hotmail.com>
2017-10-16 20:41:35 -0500

    update factory, controller js files

---
+++ js/supplies-controller.js
@@ -0,0 +1,5 @@
+/* --Read data using JSON.parse-- */
+
+//Create a constant named "storedSupplyInventory" to read data ("supplyInventory") in memory
+const storedSupplyInventory = JSON.parse(localStorage.getItem("supplyInventory"))
+
