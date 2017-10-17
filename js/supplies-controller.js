c02f38b6fe526c4069ebfcf2b160daf17df98d61
eagobert <eagobert@hotmail.com>
2017-10-17 14:12:44 -0500

    Add comments to controller file

--- js/supplies-controller.js
+++ js/supplies-controller.js
@@ -1,7 +1,7 @@
-//
+//Create const named "supplyElement" to print supplies to the website.  The computer creates an empty array, therefore the index should start at [0] to ensure the file is read starting at the initial dom element created.  In this case, the class name "supplyInventory" I created on supplies.html
 const supplyElement = document.getElementsByClassName("supplyInventory")[0];
 
-/* --Read data using JSON.parse-- */
+/* --Start read data using JSON.parse-- */
 
 //Create a constant named "storedSupplyInventory" to read data ("supplyInventory") in memory
 const storedSupplyInventory = localStorage.getItem("supplyInventory")
@@ -9,9 +9,9 @@ const storedSupplyInventory = localStorage.getItem("supplyInventory")
 //Create a variable named "supplies" to store the read data in "storedSupplyInventory"
 let supplyObject = JSON.parse(storedSupplyInventory)
 
+/* --End read data using JSON.parse-- */
 
-//Create a 
-
+//Create a "for in" loop to 
 for(let key in supplyObject) {
 
     console.log(key + ": " + supplyObject[key].length);
